eca579db1cddc06fea6505451c07c094ced2c8c2
spirow73 <67395826+spirow73@users.noreply.github.com>
2024-02-22 19:31:57 +0100

    Añadido proptypes y arreglados errores

--- DetailsScreen.js
+++ DetailsScreen.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-
+import PropTypes from "prop-types";
+import React from "react";
+import { Text, View } from "react-native";
 
 const DetailsScreen = ({ route }) => {
-    const { textoUsuario } = route.params;
+  const { textoUsuario } = route.params;
 
-    const cleanText = textoUsuario.replace('Escribe algo aquí: ', '');
+  const cleanText = textoUsuario.replace("Escribe algo aquí: ", "");
 
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Detalles de la vista</Text>
-            <Text>Texto recibido: {cleanText}</Text>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Detalles de la vista</Text>
+      <Text>Texto recibido: {cleanText}</Text>
+    </View>
+  );
 };
 
 DetailsScreen.propTypes = {
-    route: PropTypes.shape({
-        params: PropTypes.shape({
-            textoUsuario: PropTypes.string.isRequired
-        }).isRequired
-    }).isRequired
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      textoUsuario: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
-
 export default DetailsScreen;
-
