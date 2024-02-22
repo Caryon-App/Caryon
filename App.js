984524053ae98cd133e8e4d5c726f1c4dd5a4752
spirow73 <67395826+spirow73@users.noreply.github.com>
2024-02-22 17:21:40 +0100

    Pruebas con el boton
    Posibilidad de escrbir el texto y que cuando presiones el boton se vea en la alerta

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react"; // Importa useState
+import { Alert, Button, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  // Define textoUsuario como un estado con useState
+  const [textoUsuario, setTextoUsuario] = useState("Escribe aquí...");
+
   return (
-    <><View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Try editing me! 🎉</Text>
-      <View style={styles.buttonContainer}></View>
-      <Button title="Presiona aquí" onPress={() => Alert.alert('Botón presionado')} />
-    </View><StatusBar style="auto" /></>
+    <>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Try editing me! 🎉</Text>
+        <View style={styles.buttonContainer}></View>
+        <Button
+          title="Presiona aquí"
+          onPress={() =>
+            Alert.alert("Botón presionado", "Texto ingresado: " + textoUsuario)
+          }
+        />
+        {/* Actualiza textoUsuario con setTextoUsuario */}
+        <TextInput
+          value={textoUsuario}
+          onChangeText={(text) => setTextoUsuario(text)}
+        ></TextInput>
+      </View>
+      <StatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonContainer: {
     margin: 20,
